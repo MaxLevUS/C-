53cbd72dfe9811c063090c495da123dbf3994a47
Maksim Levanovich <maxlevanovich@gmail.com>
2025-01-10 18:23:36 -0600

    C# basic

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace project {
+
+
+    class Program {
+        
+        static void Main(){
+            // int number;
+            // number = -10;
+            // Console.WriteLine(number);
+            // number = 10 + 15;
+            // Console.WriteLine(number);
+            // int plusNumber = number +100;
+            // Console.WriteLine(plusNumber);
+
+            // float myNum = 2.4f;
+            // double yourNum = 3.55d;
+            // float theirNum = 56.8f;
+            // Console.WriteLine(yourNum + theirNum);
+            // Console.WriteLine(yourNum);
+
+            // byte num1 = 9, num2 = 10;
+            // int usersNum = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Num1: " + num1);
+            // Console.WriteLine("Num2: " + num2);
+            // int total = num1 + num2 + usersNum;
+            // Console.WriteLine("Num2 + Num 1 + UsersNum = " + total);
+
+
+
+            // Console.Write("Insert the number: ");
+            // // user_input = Convert.ToDouble(Console.ReadLine());
+
+            // float user_input = float.Parse(Console.ReadLine());
+
+            // float result;
+            // result = user_input + 10f;
+            
+
+            // Console.WriteLine("Variable: " + user_input);
+            // Console.WriteLine("Result: " + result);
+
+            // float doubledResult = result *= 2f;
+            // Console.WriteLine("Result * 2: " + doubledResult);
+
+            // doubledResult++;
+            // Console.WriteLine("Result * 2: " + doubledResult);
+
+            // Console.WriteLine(Math.PI);
+            // Console.WriteLine(Math.Ceiling(4.11)); //Округляет до большего в данном случае округляет к 5. 
+            // Console.WriteLine(Math.Floor(4.99)); // Округляет к меньшему - 4.
+            // Console.WriteLine(Math.Round(4.6)); //округляет к ближайшему значению после точки, в данном случае к 5.
+            // Console.WriteLine(Math.Round(4.5)); // 4
+            // Console.WriteLine(Math.Min(4, 5)); // 4
+            // Console.WriteLine(Math.Max(4, 5)); // 5
+            // Console.WriteLine(Math.Pow(4, 5)); // 1024
+
+
+// Ниже написали программу высчитывающую площадь круга при введенном пользователем радиусе...
+            // System.Console.WriteLine("Insert radius of circle: ");
+            // double radius = Convert.ToDouble(Console.ReadLine());
+            // double area = Math.PI * Math.Pow(radius, 2);
+            // System.Console.WriteLine("Площадь круга с радиусовм {0} равна {1}", radius, area);
+    
+        }
+    }
+}
